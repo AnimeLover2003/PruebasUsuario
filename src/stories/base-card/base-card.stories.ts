fc8d35cab978e26f72865b3374c6963d5de8bff4
IsaakuTK <isaaccallecardozo@gmail.com>
2025-06-10 22:35:25 -0500

    acabe

---
+++ src/stories/base-card/base-card.stories.ts
@@ -0,0 +1,31 @@
+// base-card.stories.ts
+import type { Meta, StoryObj } from '@storybook/angular';
+import { BaseCardComponent } from '../../app/components/base-card/base-card.component';
+
+
+// Configuración básica del Story
+const meta: Meta<BaseCardComponent> = {
+  title: 'Components/BaseCard', // Ruta en el panel de Storybook
+  component: BaseCardComponent,
+  tags: ['autodocs'], // Genera documentación automática
+  render: (args) => ({
+    props: args,
+    template: `
+      <app-base-card>
+        {{ content }}
+      </app-base-card>
+    `,
+  }),
+  argTypes: {
+    // Define controles personalizados (opcional)
+    content: {
+      control: 'text',
+      description: 'Contenido proyectado dentro de la card',
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<BaseCardComponent>;
+
+export const Default: Story = {};
