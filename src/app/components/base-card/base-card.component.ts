0334d33aabd41bab8f5419feabf34a629e15b485
IsaakuTK <isaaccallecardozo@gmail.com>
2025-06-11 10:44:36 -0500

    acabe

---
+++ src/app/components/base-card/base-card.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderMomentosComponent } from '../header/header-momentos/header-momentos.component';
+import { InputComponent } from '../input/input.component';
+import { CheckboxTextComponent } from '../Checkbox/CheckboxText/CheckboxText.component';
+import { ButtonComponent } from '../button/button.component';
+
+@Component({
+  selector: 'app-base-card',
+  standalone: true,  
+  imports: [CommonModule, HeaderMomentosComponent, InputComponent, CheckboxTextComponent, ButtonComponent],
+  templateUrl: './base-card.component.html',
+  styleUrl: './base-card.component.scss'
+})
+export class BaseCardComponent {
+}
